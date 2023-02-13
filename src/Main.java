a4cb583f0e69b9222e4785a0de6a1ef71977ae9a
HellHorseman <duke.sheffer@yandex.ru>
2023-02-13 19:22:31 +0400

    New branch + add task 1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,48 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        task1();
+        task2();
+        task3();
+        task4();
+        task5();
+        task6();
+        task7();
+    }
+
+    public static void task1 () {
+        System.out.println("Задача 1");
+    int age = 18;
+        System.out.println("Если возраст человека равен " + age + " , то ");
+    if (age >= 18) {
+        System.out.println(" он совершеннолетний");
+    }
+    else {
+        System.out.println(" он не достиг совершеннолетия, нужно подождать");
+    }
+    }
+
+    public static void task2 () {
+        System.out.println("Задача 2");
+
+    }
+    public static void task3 () {
+        System.out.println("Задача 3");
+
+    }
+    public static void task4 () {
+        System.out.println("Задача 4");
+
+    }
+    public static void task5 () {
+        System.out.println("Задача 5");
+
+    }
+    public static void task6 () {
+        System.out.println("Задача 6");
+
+    }
+    public static void task7 () {
+        System.out.println("Задача 7");
+
     }
 }
