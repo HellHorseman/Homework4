ac60529add74568fe6b460aff627332ee38d8a71
HellHorseman <duke.sheffer@yandex.ru>
2023-02-13 22:30:56 +0400

    add task 6

--- src/Main.java
+++ src/Main.java
@@ -48,15 +48,15 @@ public class Main {
     if (age >= 2 && age <= 6) {
         System.out.println("то ему нужно ходить в детский сад");
         }
-    else if (age >= 7 && age <= 18) {
+        else if (age >= 7 && age <= 18) {
         System.out.println("то ему нужно ходить в школу");
-        }
-    else if (age > 18 && age < 24) {
-        System.out.println("то ему нужно ходить в университет");
-        }
-    else if (age > 24) {
-        System.out.println("то ему нужно ходить на работу");
-        }
+            }
+            else if (age > 18 && age < 24) {
+            System.out.println("то ему нужно ходить в университет");
+                }
+                else if (age > 24) {
+                System.out.println("то ему нужно ходить на работу");
+                    }
     }
     public static void task5 () {
         System.out.println("Задача 5");
@@ -65,16 +65,30 @@ public class Main {
     if (age <= 5) {
         System.out.println("то ему нельзя кататься на аттракционе");
         }
-    else if (age > 5 && age <= 14) {
+        else if (age > 5 && age <= 14) {
         System.out.println("то можно кататься на аттракционе в сопровождении взрослого");
-        }
-    else {
-        System.out.println("то можно кататься на аттракционе без сопровождения взрослого");
-    }
+            }
+            else {
+            System.out.println("то можно кататься на аттракционе без сопровождения взрослого");
+                }
     }
     public static void task6 () {
         System.out.println("Задача 6");
-
+    int totalWagonPlace = 102;
+    int sittingPlace = 60;
+    int standingPlace = totalWagonPlace - sittingPlace;
+    int passengers = 55;
+    int lastSittingPlace = sittingPlace - passengers;
+    int lastStandingPlace = standingPlace - (passengers - sittingPlace);
+    if (passengers >= 0 && passengers <= 60) {
+        System.out.println("Если пассажиров " + passengers + ", то сидячих мест " + lastSittingPlace + ", стоячих мест " + standingPlace);
+        }
+        else if (passengers > 60 && passengers < 102) {
+        System.out.println("Если пассажиров " + passengers + ", то сидячих мест 0, стоячих мест " + lastStandingPlace);
+            }
+            else {
+            System.out.println("Мест нет!");
+                }
     }
     public static void task7 () {
         System.out.println("Задача 7");
