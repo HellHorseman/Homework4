7dbb8210e88eb1a5101ac125d8995b6bec74076e
HellHorseman <duke.sheffer@yandex.ru>
2023-02-13 19:32:40 +0400

    add task 2

--- src/Main.java
+++ src/Main.java
@@ -23,7 +23,16 @@ public class Main {
 
     public static void task2 () {
         System.out.println("Задача 2");
-
+    int temperature = 3;
+    if (temperature <= -5) {
+        System.out.println("На улице " + temperature + " градусов, нужно надеть шапку");
+    }
+    if (temperature >= 5) {
+        System.out.println("На улице " + temperature + " градусов, можно идти без шапки");
+    }
+    else {
+        System.out.println("На улице " + temperature + " градусов, сиди дома :р");
+    }
     }
     public static void task3 () {
         System.out.println("Задача 3");
